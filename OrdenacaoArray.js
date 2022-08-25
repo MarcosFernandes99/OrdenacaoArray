c4032ef3a3a7b9064901b36c2f0dfe5e86c79104
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 14:29:03 -0300

    Finalizado algoritmo de ordenação de array excluindo os números 0

--- OrdenacaoArray.js
+++ OrdenacaoArray.js
@@ -1,8 +1,9 @@
-// DESAFIO ORDENAÇÃO ARRAY
+// DESAFIO ORDENAÇÃO ARRAY - SE TIVER NUMERO 0 DENTRO DO ARRAY, REMOVE-LOS
 
 var arrayNumero = []
 var contador = 0
 var opcao = 0
+var arrayAux = []
 
 var continuar = true
 while(continuar){
@@ -14,4 +15,30 @@ while(continuar){
         continuar = false
     }
 }
+
+console.log(arrayNumero)
+
+for(var atual = 0; atual < contador - 1; atual++){
+    for(var seguinte = atual + 1; seguinte < contador; seguinte++){
+        if(arrayNumero[atual] == 0){
+            arrayNumero[atual] = arrayNumero[seguinte]
+            arrayNumero[seguinte] = 0
+    }
+}
+}
+console.log("")
+console.log(`1º passo, joguei os 0 para o final`)
+console.log(arrayNumero)
+
+for(var index = 0; index < contador; index++){
+    if(arrayNumero[index] != 0){
+        arrayAux[index] = arrayNumero[index]
+    }
+}
+arrayNumero = arrayAux
+console.log("")
+console.log(`2º passo, usei um array auxiliar para extrair os números diferentes de zero`)
+console.log(arrayNumero)
+
+
 
